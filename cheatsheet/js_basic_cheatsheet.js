0f5e1f7b4513660426f00b5eba7d8e1e536f5f61
Muramasa <muramasa9.info@gmail.com>
2020-12-21 21:13:19 +0900

    データ型

--- cheatsheet/js_basic_cheatsheet.js
+++ cheatsheet/js_basic_cheatsheet.js
@@ -1,6 +1,6 @@
 // htmlに書くとき
 <script>
-  'use scrict'; {/* use〜必ず書く */}
+  'use scrict'; {/* エラーが発見しやすい */}
 {/* documentは書類全般、getElementById('')でID指定 */}
 {/* ddEventListener('動作', () => {実行内容}は動作したら実行内容のイベントを追加してという指示 */}
   document.getElementById('target').addEventListener('click', () => {
@@ -20,7 +20,7 @@
 
 
 // 定数を使っても書ける
-//【const 定数名 = 値】
+//【const 定数名 = 値;】
 const target = document.getElementById("target");
 
 target.addEventListener("click", () => {
@@ -55,3 +55,57 @@ for (let i = 0; i < 10; i++) { // iは0スタート、10までiに1ずつ足し�
 
   document.body.appendChild(div);
 }
+
+
+
+
+// 別ファイルから呼び出す
+<script src="js/main.js"></script>
+
+
+
+// 文字の記載方法 option + ¥ でバックスラッシュ
+console.log('it\'s me!'); // バックスラッシュで'を表示
+console.log("it's me!"); // ””で囲むとOK
+// \n=改行 \t=タブ
+console.log('hello' + 'world'); // 文字を足すこともできる
+
+
+
+
+// 定数の書き方【const 定数名 = 値;】
+const price = 150;
+console.log(price * 140);
+console.log(price * 160);
+
+price = 170; // エラー。定数は再代入はできない
+console.log(price * 140);
+
+// 変数の書き方【let 定数名 = 値;】
+let price = 150;
+console.log(price * 140);
+console.log(price * 160);
+
+price = 170; // OK。変数は再代入できる！
+console.log(price * 140);
+
+price += 100; // priceに100を足す
+price *= 2 // priceかける２
+price += 1; // pricc +1
+price++; // pricc +1
+price--; // pricc -1
+
+// 【定数・変数】の名前のルール
+// 大文字と小文字は区別する
+// 数値からはじめられない
+// 使える記号【 $ _  】のみ
+
+
+
+
+// データ型の種類
+console.log(typeof 'hello'); // string
+console.log(typeof 5); // number
+console.log(typeof true); // boolean
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
