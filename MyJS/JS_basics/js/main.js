11f64bfe74cf47f4fa441904a4a8c67e3e31b291
Muramasa <muramasa9.info@gmail.com>
2020-12-29 19:36:44 +0900

    dateの種類

--- MyJS/JS_basics/js/main.js
+++ MyJS/JS_basics/js/main.js
@@ -2,17 +2,13 @@
 
 
 {
+  const d = new Date(2020, 10); // 2020/11/01 00:00:00
+  d.setHours(10, 20, 30); // 2020/11/01 10:20:30
+  d.setDate(33); // 2020/12/03 10:20:30 超えたら自動補正
+  d.setDate(d.getDate() + 3); // ３日後 2020/12/06 10:20:30
+  console.log(d);
+  // console.log(`${d.getMonth() + 1}月${d.getDate()}日`)
 
-// 【】でオブジェクトのプロパティを列挙
-  const point = { // {} で囲む
-    x: 100, // ,で区切る
-    y: 180,
-  };
 
-  const points = [
-    {x: 30, y: 20},
-    {x: 10, y: 50},
-    {x: 40, y: 40},
-  ]
-  console.log(points[1].y); // 2番目のyの値を表示
+
 }
